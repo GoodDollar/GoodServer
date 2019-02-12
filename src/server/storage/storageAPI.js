1481567be485091ac0e9d12c5000016ff2222bc5
fernandomg <fernando.greco@gmail.com>
2019-02-12 17:55:54 -0300

    Add /user/mobile endpoint to register user's mobile

--- src/server/storage/storageAPI.js
+++ src/server/storage/storageAPI.js
@@ -4,19 +4,28 @@ import passport from "passport"
 import { get } from 'lodash'
 import { type StorageAPI } from '../../imports/types'
 import { wrapAsync } from '../server-middlewares'
+import { sendOTP } from '../../imports/otp'
 
 const setup = (app: Router, storage: StorageAPI) => {
   app.post("/user/add", passport.authenticate("jwt", { session: false }), wrapAsync(async (req, res, next) => {
-    const { user, body } = req
-    await storage.addUser(body.user)
+    const { body: { user } } = req
+    await storage.addUser(user)
     res.json({ ok: 1 })
   }))
 
   app.post("/user/delete", passport.authenticate("jwt", { session: false }), wrapAsync(async (req, res, next) => {
-    const { user, body } = req
-    await storage.deleteUser(body.user)
+    const { body: { user } } = req
+    await storage.deleteUser(user)
     res.json({ ok: 1 })
   }));
-}
 
+  app.post("/user/mobile", passport.authenticate("jwt", { session: false }), wrapAsync(async (req, res, next) => {
+    const { body: { user } } = req
+
+    const [, otp] = await sendOTP(user)
+    storage.storeOTP(user, otp)
+
+    res.json({ ok: 1 })
+  }))
+}
 export default setup
