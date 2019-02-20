a3c42d3fc0010e83170cb38f67f69779db64bafb
Hadar Rottenberg <hadarro@etoro.com>
2019-02-20 15:32:27 +0200

    add: fuse poa network

--- src/server/networks.js
+++ src/server/networks.js
@@ -8,6 +8,11 @@ const networks = {
     network_id: 4447,
     httpWeb3Provider: 'http://localhost:9545/',
     websocketWeb3Provider: 'wss://localhost:9545/ws'
+  },
+  fuse: {
+    network_id: 121,
+    httpWeb3Provider: 'https://rpc.fuse.io/',
+    websocketWeb3Provider: 'wss://explorer.fuse.io/socket/websocket'
   }
 }
 
