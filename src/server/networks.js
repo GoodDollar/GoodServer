fbec63e7cd3353507bee50e96933f11b2d506baf
Sebastián Galiano <sgaliano@gmail.com>
2019-02-22 17:18:35 -0300

    Add network configuration for Ganache CLI

--- src/server/networks.js
+++ src/server/networks.js
@@ -4,6 +4,11 @@ const networks = {
     httpWeb3Provider: `https://kovan.infura.io/v3/${process.env.INFURA_API}`,
     websocketWeb3Provider: 'wss://kovan.infura.io/ws'
   },
+  ganache: {
+    network_id: 6000,
+    httpWeb3Provider: 'http://localhost:8545/',
+    websocketWeb3Provider: 'wss://localhost:8545/ws'
+  },
   truffle: {
     network_id: 4447,
     httpWeb3Provider: 'http://localhost:9545/',
