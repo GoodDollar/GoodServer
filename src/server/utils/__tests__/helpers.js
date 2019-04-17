2982cbcb1e6e5b29fa03ca4d22bce91f680a7df3
Your Name <dminones@gmail.com>
2019-04-17 12:23:38 -0300

    improving tests

--- src/server/utils/__tests__/helpers.js
+++ src/server/utils/__tests__/helpers.js
@@ -2,44 +2,39 @@
 import { onlyInEnv } from '../helpers'
 import MockExpressRequest from 'mock-express-request'
 import MockExpressResponse from 'mock-express-response'
-import type { $Request, $Response, NextFunction } from 'express'
 
 describe('storageAPI', () => {
-  let onlyTest, onlyProd, onlyProdAndTest
-  beforeAll(() => {
-    onlyTest = onlyInEnv('test')
-    onlyProd = onlyInEnv('production')
-    onlyProdAndTest = onlyInEnv('production', 'test')
-  })
-
   test('use onlyProd should return {ok:1}', done => {
-    const response: $Response = {
+    const onlyProd = onlyInEnv('production')
+    const response = {
       ...new MockExpressResponse(),
       json: data => {
         expect(data).toEqual({ ok: 1 })
         done()
       }
     }
-    const req: $Request = new MockExpressRequest()
-    const next: NextFunction = () => null
+    const req = new MockExpressRequest()
+    const next = () => null
     onlyProd(req, response, next)
   })
 
   test('use onlyTest should execute next', done => {
-    const response: $Response = new MockExpressResponse()
-    const req: $Request = new MockExpressRequest()
-    const next: NextFunction = () => {
-      done()
-    }
-    onlyProdAndTest(req, response, next)
-  })
-
-  test('use onlyProdAndTest should execute next', done => {
-    const response: $Response = new MockExpressResponse()
-    const req: $Request = new MockExpressRequest()
-    const next: NextFunction = () => {
+    const onlyTest = onlyInEnv('test')
+    const response = new MockExpressResponse()
+    const req = new MockExpressRequest()
+    const next = () => {
       done()
     }
     onlyTest(req, response, next)
+  })
+
+  test('use onlyProdAndTest should execute next', done => {
+    const onlyProdAndTest = onlyInEnv('production', 'test')
+    const response = new MockExpressResponse()
+    const req = new MockExpressRequest()
+    const next = () => {
+      done()
+    }
+    onlyProdAndTest(req, response, next)
   })
 })
