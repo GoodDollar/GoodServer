1dd3e0af0fab5d54eabe0a730dce8634a5626afb
Hadar Rottenberg <hadar@gooddollar.org>
2019-04-29 11:43:48 +0300

    add: create contact for user on mautic

--- src/server/storage/storageAPI.js
+++ src/server/storage/storageAPI.js
@@ -4,14 +4,20 @@ import passport from 'passport'
 import { type StorageAPI } from '../../imports/types'
 import { wrapAsync } from '../utils/helpers'
 import { defaults } from 'lodash'
+import { UserRecord } from '../../imports/types'
+import { Mautic } from '../mautic/mauticAPI'
 const setup = (app: Router, storage: StorageAPI) => {
   app.post(
     '/user/add',
     passport.authenticate('jwt', { session: false }),
     wrapAsync(async (req, res, next) => {
       const { body } = req
-      const user = defaults(body.user, { identifier: req.user.loggedInAs })
+      const user: UserRecord = defaults(body.user, { identifier: req.user.loggedInAs })
       await storage.addUser(user)
+      //if adduser went ok then we create a contact and update the user with mauticId
+      const mauticRecord = await Mautic.createContact(user)
+      storage.addUser({ mauticId: mauticRecord.contact.fields.all.id, identifier: req.user.loggedInAs })
+
       res.json({ ok: 1 })
     })
   )
