b2fb3fa7cabc2d2284abf53e9ffdc3a2ab49276b
hadarbmdev <hadarbmdev@gmail.com>
2019-05-08 16:17:31 +0300

    handle error messages from AFP - WIP

--- src/server/face-recognition/livenesstest-middleware.js
+++ src/server/face-recognition/livenesstest-middleware.js
@@ -23,16 +23,24 @@ const setup = (app: Router) => {
     wrapAsync(async (req, res, next) => {
       const log = req.log.child({ from: 'livenesstest' })
       const { body, files, user } = req
+      log.info({ files })
       const livenessApi = conf.faceRecoServer + '/users'
-      log.debug(`Sending request to: ${livenessApi}`)
       let form = new FormData()
       let facemapfile = _.find(files, { fieldname: 'facemap' }).path
-      let auditTrailImagefile = _.find(files, { fieldname: 'auditTrailImage' }).path
+      let auditTrailImagefile = _.find(files, { fieldname: 'audit_trail_image' }).path
       let facemap = fs.createReadStream(facemapfile)
       let auditTrailImage = fs.createReadStream(auditTrailImagefile)
+      log.debug({ user })
+      log.debug({ body })
+
       form.append('name', user.fullName)
-      form.append('email', user.email)
-      form.append('session_id', body.sessionId)
+      form.append(
+        'email',
+        Math.random()
+          .toString(36)
+          .substring(7) + user.email
+      )
+      form.append('session_id', body.session_id)
       form.append('facemap', facemap)
       form.append('audit_trail_image', auditTrailImage)
 
@@ -45,10 +53,21 @@ const setup = (app: Router) => {
             'Content-Type': `multipart/form-data; boundary=${form._boundary}`
           }
         })
-        res.json({ ok: 1, response: response })
-      } catch (e) {
-        log.error(e)
-        res.json({ ok: 0 })
+
+        res.json({ ok: 1, livenessFailed: response.livenessFailed, duplicates: response.duplicates })
+      } catch (error) {
+        if (error.response) {
+          log.error(error.response.data)
+          res.json({ ok: 0, livenessFailed: error.response.livenessFailed, duplicates: error.response.duplicates })
+        } else if (error.request) {
+          // TODO: handle general error to the client - The request was made but no response was received
+
+          log.error(error.request)
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          // TODO: handle general error to the client
+          log.error('Error', error.message)
+        }
       }
     })
   )
