df5c85d7d6df29eb19d688aa9be765b9b61dc610
fernandomg <fernando.greco@gmail.com>
2019-05-30 14:59:39 -0300

    newrelic setup

--- src/server/app.js
+++ src/server/app.js
@@ -2,6 +2,7 @@ import path from 'path'
 import express from 'express'
 import middlewares from './server-middlewares'
 const app = express()
+require('newrelic')
 
 app.use(express.static('public'))
 middlewares(app, 'prod')
