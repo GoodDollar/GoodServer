9dd27150773c9db72c9ed0dcc4ca38f73fa6f0d7
fernandomg <fernando.greco@gmail.com>
2019-05-30 22:47:57 -0300

    Revert "move newrelic to top"
    This reverts commit c2e06c59

--- src/server/app.js
+++ src/server/app.js
@@ -1,8 +1,8 @@
-import 'newrelic'
 import path from 'path'
 import express from 'express'
 import middlewares from './server-middlewares'
 const app = express()
+require('newrelic')
 
 app.use(express.static('public'))
 middlewares(app, 'prod')
