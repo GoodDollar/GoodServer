ea131e526e9e8714095e486be97b0e15e1227411
fernandomg <fernando.greco@gmail.com>
2019-05-31 14:00:09 -0300

    setup newrelic

--- src/server/server-dev.js
+++ src/server/server-dev.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
+import 'newrelic'
 import path from 'path'
 import webpack from 'webpack'
 import webpackDevMiddleware from 'webpack-dev-middleware'
