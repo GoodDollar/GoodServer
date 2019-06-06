cca70c516663c555d96aca8aba17a02cc366dd3c
Hadar Rottenberg <hadar@gooddollar.org>
2019-06-06 21:02:40 +0300

    add: remove heapdump, fix heroku R10 crash?

--- src/server/server-prod.js
+++ src/server/server-prod.js
@@ -5,16 +5,6 @@ import conf from './server.config'
 import { GunDBPublic } from './gun/gun-middleware'
 import app from './app'
 
-const DIST_DIR = __dirname
-
-const HTML_FILE = path.join(DIST_DIR, 'index.html')
-
-app.use(express.static(DIST_DIR))
-
-app.get('*', (req, res) => {
-  res.sendFile(HTML_FILE)
-})
-
 const PORT = process.env.PORT || 3000
 const server = app.listen(PORT, () => {
   console.log(`App listening to ${PORT}....`)
