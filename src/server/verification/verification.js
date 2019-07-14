1585342db917565ebc6af5a4a5bb8802951f1dcf
Hadar Rottenberg <hadar@gooddollar.org>
2019-07-14 17:23:25 +0300

    wip: test why otp fails

--- src/server/verification/verification.js
+++ src/server/verification/verification.js
@@ -57,7 +57,7 @@ class Verifications implements VerificationAPI {
    */
   async verifyMobile(user: UserRecord, verificationData: { otp: string }): Promise<boolean | Error> {
     const otp = await GunDBPrivate.getUserField(user.identifier, 'otp')
-
+    this.log.debug('verifyMobile:', { userId: user.identifier, otp })
     if (otp) {
       if (verificationData.otp === otp.code) {
         if (otp.expirationDate < Date.now()) {
