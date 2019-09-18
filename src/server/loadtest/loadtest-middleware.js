58968f744322338e7e7ac753121ff7e7134bb80b
alexeykosinski <alexey.kosinski@nordwhale.com>
2019-09-18 17:09:53 +0300

    revert load test middlware

--- src/server/loadtest/loadtest-middleware.js
+++ src/server/loadtest/loadtest-middleware.js
@@ -4,8 +4,6 @@ import passport from 'passport'
 import { wrapAsync, onlyInEnv } from '../utils/helpers'
 import { recoverPublickey } from '../utils/eth'
 import AdminWallet from '../blockchain/AdminWallet'
-import logger from '../../imports/pino-logger'
-const log = logger.child({ from: 'loadtestWhitelistUser' })
 
 const setup = (app: Router) => {
   /**
@@ -16,22 +14,17 @@ const setup = (app: Router) => {
     passport.authenticate('jwt', { session: false }),
     onlyInEnv('test', 'development'),
     wrapAsync(async (req, res, next) => {
-      try {
-        const { body } = req
-        const gdSignature = body.gdSignature
-        const nonce = body.nonce
-        const msg = 'Login to GoodDAPP'
-        const gdPublicAddress = recoverPublickey(gdSignature, msg, nonce)
-        log.info('#############################################')
-        log.info(gdPublicAddress)
-        log.info('#############################################')
-        const hash = await AdminWallet.whitelistUser(gdPublicAddress, body.profilePublickey)
-        log.info('hash', hash)
-        res.json({ ok: 1 })
-      } catch (e) {
-        log.error('whitelistUser test error', e)
-        throw new Error(e)
-      }
+      const { body } = req
+      const gdSignature = body.gdSignature
+      const nonce = body.nonce
+      const msg = 'Login to GoodDAPP'
+      const gdPublicAddress = recoverPublickey(gdSignature, msg, nonce)
+      console.log('#############################################')
+      console.log(gdPublicAddress)
+      console.log('#############################################')
+      const hash = await AdminWallet.whitelistUser(gdPublicAddress, body.profilePublickey)
+      console.log('xxxxxxxx hash', hash)
+      res.json({ ok: 1 })
     })
   )
 }
