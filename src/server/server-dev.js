312e5233975ef955f7830c641d74c1849379d3dc
alexeykosinski <alexey.kosinski@nordwhale.com>
2019-09-27 10:11:23 +0300

    setup node cluster

--- src/server/server-dev.js
+++ src/server/server-dev.js
@@ -1,51 +1,57 @@
 /* eslint-disable import/no-extraneous-dependencies */
+import throng from 'throng'
 import 'newrelic'
 import webpack from 'webpack'
 import webpackHotMiddleware from 'webpack-hot-middleware'
 import config from '../../webpack.dev.config'
 import conf from './server.config'
 import { GunDBPublic } from './gun/gun-middleware'
-import app from './app'
 
-process.on('uncaughtException', (err, origin) => {
-  console.log(`Caught exception: ${err}\n` + `Exception origin: ${origin}`)
-  console.log(err.stack)
-  process.exit(-1)
-})
+function start() {
+  const app = require('./app').default
 
-process.on('unhandledRejection', (reason, promise) => {
-  console.log('Unhandled Rejection at:', promise, 'reason:', reason)
-  // Application specific logging, throwing an error, or other logic here
-})
-const compiler = webpack(config)
-// app.use(webpackDevMiddleware(compiler, {
-//   publicPath: config.output.publicPath
-// }))
+  process.on('uncaughtException', (err, origin) => {
+    console.log(`Caught exception: ${err}\n` + `Exception origin: ${origin}`)
+    console.log(err.stack)
+    process.exit(-1)
+  })
 
-app.use(webpackHotMiddleware(compiler))
+  process.on('unhandledRejection', (reason, promise) => {
+    console.log('Unhandled Rejection at:', promise, 'reason:', reason)
+    // Application specific logging, throwing an error, or other logic here
+  })
+  const compiler = webpack(config)
+  // app.use(webpackDevMiddleware(compiler, {
+  //   publicPath: config.output.publicPath
+  // }))
 
-// const DIST_DIR = __dirname
-// const HTML_FILE = path.join(DIST_DIR, 'index.html')
-// app.get("*", (req, res, next) => {
-//   compiler.outputFileSystem.readFile(HTML_FILE, (err, result) => {
-//     if (err) {
-//       return next(err)
-//     }
-//     res.set("content-type", "text/html")
-//     res.send(result)
-//     res.end()
-//     return false
-//   })
-// })
+  app.use(webpackHotMiddleware(compiler))
 
-console.log({ conf })
-const PORT = conf.port || 8080
+  // const DIST_DIR = __dirname
+  // const HTML_FILE = path.join(DIST_DIR, 'index.html')
+  // app.get("*", (req, res, next) => {
+  //   compiler.outputFileSystem.readFile(HTML_FILE, (err, result) => {
+  //     if (err) {
+  //       return next(err)
+  //     }
+  //     res.set("content-type", "text/html")
+  //     res.send(result)
+  //     res.end()
+  //     return false
+  //   })
+  // })
 
-const server = app.listen(PORT, () => {
-  console.log(`App listening to ${PORT}....`)
-  console.log('Press Ctrl+C to quit.')
-})
-GunDBPublic.init(server, conf.gundbPassword, 'publicdb', conf.gunPublicS3).catch(e => {
-  console.error(e)
-  process.exit(-1)
-})
+  console.log({ conf })
+  const PORT = conf.port || 8080
+
+  const server = app.listen(PORT, () => {
+    console.log(`App listening to ${PORT}....`)
+    console.log('Press Ctrl+C to quit.')
+  })
+  GunDBPublic.init(server, conf.gundbPassword, 'publicdb', conf.gunPublicS3).catch(e => {
+    console.error(e)
+    process.exit(-1)
+  })
+}
+
+throng({ workers: conf.webConcurrency, lifetime: Infinity }, start)
