31e3dd2c89d69dea814aba47592277e04f4c6979
alexeykosinski <alexey.kosinski@nordwhale.com>
2019-09-27 10:18:08 +0300

    add to prod

--- src/server/server-prod.js
+++ src/server/server-prod.js
@@ -3,31 +3,36 @@ import path from 'path'
 import express from 'express'
 import conf from './server.config'
 import { GunDBPublic } from './gun/gun-middleware'
-import app from './app'
+import throng from 'throng'
 
-process.on('uncaughtException', (err, origin) => {
-  console.log(`Caught exception: ${err}\n` + `Exception origin: ${origin}`)
-  process.exit(-1)
-})
+function start() {
+  const app = require('./app').default
+  process.on('uncaughtException', (err, origin) => {
+    console.log(`Caught exception: ${err}\n` + `Exception origin: ${origin}`)
+    process.exit(-1)
+  })
 
-process.on('unhandledRejection', (reason, promise) => {
-  console.log('Unhandled Rejection at:', promise, 'reason:', reason)
-  // Application specific logging, throwing an error, or other logic here
-})
-const DIST_DIR = __dirname
+  process.on('unhandledRejection', (reason, promise) => {
+    console.log('Unhandled Rejection at:', promise, 'reason:', reason)
+    // Application specific logging, throwing an error, or other logic here
+  })
+  const DIST_DIR = __dirname
 
-const HTML_FILE = path.join(DIST_DIR, 'index.html')
+  const HTML_FILE = path.join(DIST_DIR, 'index.html')
 
-app.use(express.static(DIST_DIR))
+  app.use(express.static(DIST_DIR))
 
-app.get('*', (req, res) => {
-  res.sendFile(HTML_FILE)
-})
+  app.get('*', (req, res) => {
+    res.sendFile(HTML_FILE)
+  })
 
-const PORT = process.env.PORT || 3000
-const server = app.listen(PORT, () => {
-  console.log(`App listening to ${PORT}....`)
-  console.log('Press Ctrl+C to quit.')
-})
+  const PORT = process.env.PORT || 3000
+  const server = app.listen(PORT, () => {
+    console.log(`App listening to ${PORT}....`)
+    console.log('Press Ctrl+C to quit.')
+  })
 
-GunDBPublic.init(server, conf.gundbPassword, 'publicdb', conf.gunPublicS3)
+  GunDBPublic.init(server, conf.gundbPassword, 'publicdb', conf.gunPublicS3)
+}
+
+throng({ workers: conf.webConcurrency, lifetime: Infinity }, start)
