c715301bb2b7cdf2608a1e048cfb9d5c828e6ba1
Yaroslav Fedyshyn <yaroslav.fedyshyn@nordwhale.com>
2019-10-21 10:51:32 +0300

    fix: pass minutes and max count values for rate limit guard from environment variables

--- src/server/utils/requestRateLimiter.js
+++ src/server/utils/requestRateLimiter.js
@@ -1,7 +1,11 @@
 import rateLimit from 'express-rate-limit'
+import config from '../server.config'
 
-export default (t = 1, n = 3) =>
+const m = config.rateLimitMinutes
+const n = config.rateLimitRequestsCount
+
+export default () =>
   rateLimit({
-    windowMs: +t * 60 * 1000, // minutes
+    windowMs: +m * 60 * 1000, // minutes
     max: +n // limit each IP to n requests per windowMs
   })
