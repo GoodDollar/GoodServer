a2dc1bb568dcb9ef55ae733f5612c9f16e2656c2
Hadar Rottenberg <hadar@gooddollar.org>
2019-11-08 15:47:29 +0200

    refactor: log error to warn

--- src/server/login/login-middleware.js
+++ src/server/login/login-middleware.js
@@ -85,7 +85,7 @@ const setup = (app: Router) => {
       const networkId = req.body.networkId
 
       if (networkId !== Config.ethereum.network_id) {
-        log.error('/auth/eth', 'Networkd id mismatch', { client: networkId, server: Config.ethereum.network_id })
+        log.warn('/auth/eth', 'Networkd id mismatch', { client: networkId, server: Config.ethereum.network_id })
         throw new Error('Network ID mismatch')
       }
       log.debug('/auth/eth', { signature, method })
