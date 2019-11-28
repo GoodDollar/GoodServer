86ddc1ec50f19992b237f411b6e163f2981dad5f
Hadar Rottenberg <hadar@gooddollar.org>
2019-11-28 14:25:32 +0200

    Revert "add: update fuse websockets rpc"
    This reverts commit 4f3c7a9d395944728dfe19a79bb6bcae1bf5155f.

--- src/server/networks.js
+++ src/server/networks.js
@@ -15,13 +15,13 @@ const networks = {
     network_id: 121,
     web3Transport: 'HttpProvider',
     httpWeb3Provider: 'https://rpc.fuse.io/',
-    websocketWeb3Provider: 'wss://rpc.fuse.io/ws'
+    websocketWeb3Provider: 'wss://explorer-node.fuse.io/ws'
   },
   122: {
     network_id: 122,
     web3Transport: 'HttpProvider',
     httpWeb3Provider: 'https://rpc.fuse.io/',
-    websocketWeb3Provider: 'wss://rpc.fuse.io/ws'
+    websocketWeb3Provider: 'wss://explorer-node.fuse.io/ws'
   }
 }
 
