5ef9e9bf8da8b34e5714f9b7b55907dfc47873c5
Hadar Rottenberg <hadar@gooddollar.org>
2020-03-18 15:02:03 +0200

    refactor: use try catch for await

--- src/server/verification/verificationAPI.js
+++ src/server/verification/verificationAPI.js
@@ -318,18 +318,19 @@ const setup = (app: Router, verifier: VerificationAPI, storage: StorageAPI) => {
       if (conf.skipEmailVerification === false) {
         const code = generateOTP(6)
         if (!user.isEmailConfirmed || email !== currentEmail) {
-          await Mautic.sendVerificationEmail(
-            {
-              fullName: userRec.fullName,
-              mauticId: (userRec.otp && userRec.otp.tempMauticId) || userRec.mauticId
-            },
-            code
-          ).catch(e => {
-            log.error('failed sending email verification to user:', e.message, { userRec })
+          try {
+            await Mautic.sendVerificationEmail(
+              {
+                ...userRec,
+                mauticId: (userRec.otp && userRec.otp.tempMauticId) || userRec.mauticId
+              },
+              code
+            )
+            log.debug('sent new user email validation code', code)
+          } catch (e) {
+            log.error('failed sending email verification to user:', e.message, { userRec, code })
             throw e
-          })
-
-          log.debug('send new user email validation code', code)
+          }
         }
 
         // updates/adds user with the emailVerificationCode to be used for verification later and with mauticId
