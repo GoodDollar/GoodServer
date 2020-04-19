99ec535baba2b71405d685fe48e54aaa80002b33
Hadar Rottenberg <hadar@gooddollar.org>
2020-04-19 08:43:45 +0300

    fix: create indexes correctly

--- src/server/db/mongo-db.js
+++ src/server/db/mongo-db.js
@@ -4,6 +4,6 @@ import config from '../server.config'
 
 const { uri } = config.mongodb
 
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true })
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, autoIndex: true, useFindAndModify: false })
 
 export default mongoose
