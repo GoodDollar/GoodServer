a3d4d7e4c9c7305282d1902934b43c1254788d9d
Hadar Rottenberg <hadar@gooddollar.org>
2020-05-31 12:36:45 +0300

    add: upgrade database to hashes

--- src/server/db/mongo/models/constants.js
+++ src/server/db/mongo/models/constants.js
@@ -1,4 +1,4 @@
 import config from '../../../server.config'
 export const MODEL_USER_PRIVATE = 'userprivate_' + (process.env.APP_NAME || config.network)
 export const MODEL_WALLET_NONCE = 'walletnonce'
-export const MODEL_PROPERTIES = 'server_properties'
+export const MODEL_PROPERTIES = 'server_properties_' + (process.env.APP_NAME || config.network)
