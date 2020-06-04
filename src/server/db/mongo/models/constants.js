e99a9473382dacc97091e6c530826689412aaee1
sirpy <hadar@gooddollar.org>
2020-06-04 09:28:31 +0300

    Update src/server/db/mongo/models/constants.js

--- src/server/db/mongo/models/constants.js
+++ src/server/db/mongo/models/constants.js
@@ -5,4 +5,4 @@ const envId = process.env.APP_NAME || config.network
 export const MODEL_USER_PRIVATE = `userprivate_${envId}`
 export const MODEL_DELAYED_TASK = 'delayedtask'
 export const MODEL_WALLET_NONCE = 'walletnonce'
-export const MODEL_PROPERTIES = 'server_properties_' + (process.env.APP_NAME || config.network)
+export const MODEL_PROPERTIES = `server_properties_${envId}`
