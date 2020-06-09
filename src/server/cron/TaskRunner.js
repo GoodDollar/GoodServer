9a2ab628221a99c3c80ea5dac2d7a5b4698d8860
Hadar Rottenberg <hadar@gooddollar.org>
2020-06-09 22:09:37 +0300

    add: support resetting task schedule

--- src/server/cron/TaskRunner.js
+++ src/server/cron/TaskRunner.js
@@ -1,5 +1,5 @@
 import AsyncLock from 'async-lock'
-import { CronJob } from 'cron'
+import { CronJob, CronTime } from 'cron'
 import { invokeMap, map, filter } from 'lodash'
 
 import logger from '../../imports/logger'
@@ -25,12 +25,18 @@ class TaskRunner {
     const taskIdentifier = name || tasks.length
 
     tasks.push(
-      new jobFactory(schedule, async () => {
+      //we use function and not arrow so CronJob can set this context
+      new jobFactory(schedule, async function() {
         logger.info('Running cron task', { taskIdentifier })
 
         try {
-          await lock.acquire(taskIdentifier, async () => task.execute())
-          logger.info('Cron task completed', { taskIdentifier })
+          const taskResult = await lock.acquire(taskIdentifier, async () => task.execute())
+          logger.info('Cron task completed', { taskIdentifier, taskResult })
+          if (taskResult.cronTime) {
+            logger.info('Cron task setting new schedule', { schedule: taskResult.cronTime })
+            this.setTime(new CronTime(taskResult.cronTime))
+            this.start()
+          }
         } catch (exception) {
           const { message: errMessage } = exception
 
