3d29340e7bffc50194f467590fccf9cf2fb72d42
Yaroslav Fedyshyn <yaroslav.fedyshyn@nordwhale.com>
2020-07-24 11:05:40 +0300

    add: add phase configs

--- src/server/server.config.js
+++ src/server/server.config.js
@@ -24,6 +24,12 @@ const conf = convict({
     default: version,
     env: 'VERSION'
   },
+  phase: {
+    doc: 'The application release/phase version',
+    format: Number,
+    default: -1,
+    env: 'RELEASE_PHASE'
+  },
   logLevel: {
     doc: 'Log level',
     format: ['error', 'warn', 'info', 'debug', 'silent'],
@@ -510,6 +516,18 @@ const mainNetworkId = get(ContractsAddress, `${network}-mainnet.networkId`, netw
 
 conf.set('ethereumMainnet', networks[mainNetworkId])
 conf.set('ethereum', networks[networkId])
+
+// phase options - compatibility with old phase env vars (PHASE_ZERO, PHASE_ONE)
+const phase = conf.get('phase')
+
+if (phase < 0) {
+  const detectPhase = 'true' === process.env.PHASE_ONE ? 1 : 0
+
+  conf.set('phase', detectPhase)
+  conf.set('isPhaseZero', 0 === detectPhase)
+  conf.set('isPhaseOne', 1 === detectPhase)
+  conf.set('isPhaseTwo', 2 === detectPhase)
+}
 
 // GUN S3 options
 const privateS3 = process.env.GUN_PRIVATE_S3
