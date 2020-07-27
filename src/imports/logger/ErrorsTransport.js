631db4fa63416554b8cecc6d3f42867ad6bda539
Yaroslav Fedyshyn <yaroslav.fedyshyn@nordwhale.com>
2020-07-27 15:29:20 +0300

    fix: adjust building general error message sent to the sentry

--- src/imports/logger/ErrorsTransport.js
+++ src/imports/logger/ErrorsTransport.js
@@ -2,8 +2,7 @@ import * as Sentry from '@sentry/node'
 import { RewriteFrames } from '@sentry/integrations'
 import Transport from 'winston-transport'
 import { SPLAT } from 'triple-beam'
-import { forEach } from 'lodash'
-import { cloneError } from '../../server/utils/helpers'
+import { assign, forEach, get } from 'lodash'
 
 import Config from '../../server/server.config'
 
@@ -34,6 +33,16 @@ export default class ErrorsTransport extends Transport {
     }
   }
 
+  createError({ name, message, code, stack }) {
+    const error = new Error(message)
+    const stackDescriptor = Object.getOwnPropertyDescriptor('stack')
+
+    assign(error, { name, code })
+    Object.defineProperty(error, 'stack', { ...stackDescriptor, value: stack })
+
+    return error
+  }
+
   log(context) {
     const { message: generalMessage, userId, ...data } = context
 
@@ -41,16 +50,12 @@ export default class ErrorsTransport extends Transport {
     // i.e log.error('some error message')
     const [errorMessage, errorObj, extra = {}] = context[SPLAT] || []
     const dataToPassIntoLog = { generalMessage, errorMessage, errorObj, ...extra, ...data }
-
-    // we cannot modify the origin errorObj - need to clone it
-    // otherwise modification in this fn will cause some unexpected behavior in place from where it was called
-    let errorToPassIntoLog = cloneError(errorObj)
-
-    if (errorObj instanceof Error) {
-      errorToPassIntoLog.message = `${generalMessage}: ${errorObj.message}`
-    } else {
-      errorToPassIntoLog = new Error(generalMessage)
-    }
+    const errorToPassIntoLog = this.createError({
+      name: get(errorObj, 'name'),
+      message: `${generalMessage}${errorMessage ? ' :' + errorMessage : ''}`,
+      code: get(errorObj, 'code'),
+      stack: get(errorObj, 'stack')
+    })
 
     if (this.sentryInitialized) {
       Sentry.configureScope(scope => {
