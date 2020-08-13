dd5eba8aaa684135d3b3aadecb106f5da3bfd0eb
Oleksii Serdiukov <oleksii.serdiukov@nordwhale.com>
2020-08-13 13:17:44 +0300

    remove extra logging of uncaught errors

--- src/server/app.js
+++ src/server/app.js
@@ -1,22 +1,13 @@
 import express from 'express'
+
 import middlewares from './server-middlewares'
 import { EventEmitter } from 'events'
 
 EventEmitter.defaultMaxListeners = 100
+// we're logging uncaught exceptions in logger monitor so just exiting process
+process.on('uncaughtException', () => process.exit(-1))
 
 const startApp = () => {
-  if (process.env.NODE_ENV !== 'test') {
-    process.on('uncaughtException', (err, origin) => {
-      console.log(`Uncaught exception: ${err}\nException origin: ${origin}`)
-      process.exit(-1)
-    })
-
-    process.on('unhandledRejection', (reason, promise) => {
-      console.log('Unhandled Rejection at:', promise, 'reason:', reason)
-      // Application specific logging, throwing an error, or other logic here
-    })
-  }
-
   const app = express()
 
   app.use(express.static('public'))
