2eafece1c7dee47f17b622d9e621a059beeb5ef5
Oleksii Serdiukov <oleksii.serdiukov@nordwhale.com>
2020-08-14 19:26:06 +0300

    hotifx delayed tasks should use unique collection per env (as gooddev & goodqa are using the same database)

--- src/server/db/mongo/models/constants.js
+++ src/server/db/mongo/models/constants.js
@@ -3,6 +3,6 @@ import config from '../../../server.config'
 const envId = process.env.APP_NAME || config.network
 
 export const MODEL_USER_PRIVATE = `userprivate_${envId}`
-export const MODEL_DELAYED_TASK = 'delayedtask'
+export const MODEL_DELAYED_TASK = `delayedtask_${envId}`
 export const MODEL_WALLET_NONCE = 'walletnonce'
 export const MODEL_PROPERTIES = `server_properties_${envId}`
