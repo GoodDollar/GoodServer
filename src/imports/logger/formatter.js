eedbcc521bcab8e57f909cd6206f5289f31de697
Oleksii Serdiukov <oleksii.serdiukov@nordwhale.com>
2020-09-10 18:01:13 +0300

    restore original log format

--- src/imports/logger/formatter.js
+++ src/imports/logger/formatter.js
@@ -2,29 +2,21 @@
 
 import winston from 'winston'
 import errorSerializer from 'pino-std-serializers/lib/err'
-import { isError, isArray, first } from 'lodash'
-import { SPLAT } from 'triple-beam'
+import { isError, isPlainObject, omit, keys, flatten, assign, uniq } from 'lodash'
+import { SPLAT, LEVEL } from 'triple-beam'
 
 const { printf, colorize } = winston.format
 const colorizer = colorize()
 
-const getLogPayload = logRecord => {
-  const { message, uuid, ...rest } = logRecord
-  let context = rest[SPLAT]
-
-  if (isArray(context)) {
-    context = first(context)
-  }
-
-  return { message, context: { uuid, ...context } }
-}
-
 export const extended = () =>
-  printf(logRecord => {
-    const { level, timestamp, from, userId } = logRecord
-    const userString = userId ? ` ${userId}` : ''
-    const fromString = from ? ` (FROM ${from}${userString})` : ''
-    const logPayload = getLogPayload(logRecord)
+  printf(({ level, timestamp, from, userId, ...rest }) => {
+    const context = rest[SPLAT] || []
+    // objects passed to log.debug are merged with log record causing duplicates
+    // going over SPLAT (logged args) and excluding keys of the all objects logged
+    const excludeKeys = flatten(context.map(value => (isPlainObject(value) ? keys(value) : [])))
+    // excluding symbols SPLAT and LEVEL, adding logged args as 'context' property
+    const logPayload = assign(omit(rest, [SPLAT, LEVEL, ...uniq(excludeKeys)]), { context })
+    const fromString = from ? ` (FROM ${from} ${userId || ''})` : ''
 
     const stringifiedPayload = JSON.stringify(logPayload, (_, value) =>
       isError(value) ? errorSerializer(value) : value
