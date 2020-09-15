9bcc3976045190527d3f0228a6ff40ed66b874ff
sirpy <hadarr@gmail.com>
2020-09-15 14:22:07 +0300

    add: limit memory as client. server only writes.

--- src/server/gun/gun-middleware.js
+++ src/server/gun/gun-middleware.js
@@ -173,7 +173,7 @@ class GunDB implements StorageAPI {
     }
     if (this.serverMode === false) {
       log.info('Starting gun as client:', { peers: this.peers })
-      this.gun = Gun({ peers: this.peers })
+      this.gun = Gun({ peers: this.peers, memory: 25 })
     } else if (s3 && s3.secret) {
       log.info('Starting gun with S3:', { gc_delay, memory })
       this.gun = Gun({
