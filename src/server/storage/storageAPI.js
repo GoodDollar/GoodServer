12f161e72368c1afe266de5b0a22078c614c490a
Yaroslav Fedyshyn <yaroslav.fedyshyn@nordwhale.com>
2020-09-24 12:18:11 +0300

    fix: retire w3 calls from storageAPI.js /user/add

--- src/server/storage/storageAPI.js
+++ src/server/storage/storageAPI.js
@@ -79,7 +79,6 @@ const setup = (app: Router, gunPublic: StorageAPI, storage: StorageAPI) => {
           ? userRecord.isCompleted
           : {
               whiteList: false,
-              w3Record: false,
               topWallet: false
             }
       })
@@ -122,18 +121,6 @@ const setup = (app: Router, gunPublic: StorageAPI, storage: StorageAPI) => {
         signUpPromises.push(p3)
       }
 
-      // const web3RecordP = addUserSteps
-      //   .updateW3Record(userRecordWithPII, logger)
-      //   .then(r => {
-      //     logger.debug('updateW3Record success')
-      //     return r
-      //   })
-      //   .catch(e => {
-      //     logger.error('updateW3Record failed', e.message, e, { userRecordWithPII })
-      //     throw new Error('Failed adding user to w3')
-      //   })
-      // signUpPromises.push(web3RecordP)
-
       const p4 = addUserSteps
         .topUserWallet(userRecord, logger)
         .then(isTopWallet => {
@@ -167,12 +154,8 @@ const setup = (app: Router, gunPublic: StorageAPI, storage: StorageAPI) => {
         otp: {} //delete trace of mobile,email
       })
 
-      // const web3Record = await web3RecordP
-
       res.json({
         ok: 1
-        // loginToken: web3Record && web3Record.loginToken,
-        // w3Token: web3Record && web3Record.w3Token
       })
     })
   )
