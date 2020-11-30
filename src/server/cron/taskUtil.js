4f509ed92a68f1b1911817ab506ed96652f6e5d4
Aku <omerzamsky@gmail.com>
2020-11-30 18:41:56 +0200

    Update src/server/cron/taskUtil.js
    disable eslint for a line
    
    Co-authored-by: serdiukov-o-nordwhale <62208067+serdiukov-o-nordwhale@users.noreply.github.com>

--- src/server/cron/taskUtil.js
+++ src/server/cron/taskUtil.js
@@ -1,6 +1,7 @@
 //@flow
 export const DISPOSE_ENROLLMENTS_TASK = 'verification/dispose_enrollments'
 
+// eslint-disable-next-line require-await
 export const scheduleDisposalTask = async (storage, enrollmentIdentifier, executeAt): Promise<DelayedTaskRecord> => {
   await storage.cancelTasksQueued(DISPOSE_ENROLLMENTS_TASK, {
     'subject.enrollmentIdentifier': enrollmentIdentifier,
