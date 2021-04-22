a2165cc8b51de060e3c05170c5d780cc90784200
Oleksii Serdiukov <oleksii.serdiukov@nordwhale.com>
2021-04-22 14:44:12 +0300

    add replace .error to .warn logs through the whole app

--- src/server/claimQueue/claimQueueAPI.js
+++ src/server/claimQueue/claimQueueAPI.js
@@ -27,13 +27,13 @@ const ClaimQueue = {
       Mautic.updateContact(mauticId, { tags: ['claimqueue_claimed'] }, log).catch(exception => {
         const { message } = exception
 
-        log.error('Failed Mautic tagging user claimed', message, exception, { mauticId })
+        log.warn('Failed Mautic tagging user claimed', message, exception, { mauticId })
       })
 
       Mautic.addContactsToSegment([mauticId], mauticClaimQueueWhitelistedSegmentId).catch(exception => {
         const { message } = exception
 
-        log.error('Failed Mautic adding user to claim queue whitelisted segment', message, exception)
+        log.warn('Failed Mautic adding user to claim queue whitelisted segment', message, exception)
       })
     }
 
@@ -155,11 +155,11 @@ const ClaimQueue = {
     if (['test', 'development'].includes(conf.env) === false && user.mauticId) {
       if (status === 'pending') {
         Mautic.updateContact(user.mauticId, { tags: ['claimqueue_in'] }, log).catch(e => {
-          log.error('Failed Mautic tagging  user inqueue', e.message, e, { mauticId: user.mauticId })
+          log.warn('Failed Mautic tagging  user inqueue', e.message, e, { mauticId: user.mauticId })
         })
       } else {
         Mautic.updateContact(user.mauticId, { tags: ['claimqueue_autoapproved'] }, log).catch(e => {
-          log.error('Failed Mautic tagging  user autoapproved', e.message, e, { mauticId: user.mauticId })
+          log.warn('Failed Mautic tagging  user autoapproved', e.message, e, { mauticId: user.mauticId })
         })
       }
     }
