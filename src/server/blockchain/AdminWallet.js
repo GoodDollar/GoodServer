1539db0271ff74ffc5c3cdd5ca91078f626cec8f
sirpy <hadarr@gmail.com>
2021-09-01 15:30:18 +0300

    add: staging protocol v2

--- src/server/blockchain/AdminWallet.js
+++ src/server/blockchain/AdminWallet.js
@@ -1,7 +1,7 @@
 import conf from '../server.config'
 let AdminWallet
 // export default AdminWallet
-if (['staging', 'production'].includes(conf.network)) {
+if (['production'].includes(conf.network)) {
   AdminWallet = require('./AdminWalletOld').default
 } else {
   AdminWallet = require('./AdminWalletV2').default
