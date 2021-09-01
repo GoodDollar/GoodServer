be0add159e119aaf8dcff12337d1a4798f64ae1b
sirpy <hadarr@gmail.com>
2021-09-01 15:30:18 +0300

    add: staging protocol v2

--- src/server/blockchain/stakingModelTasks.js
+++ src/server/blockchain/stakingModelTasks.js
@@ -1,6 +1,6 @@
 import conf from '../server.config'
 let stakingModelTasks
-if (['staging', 'production'].includes(conf.network)) {
+if (['production'].includes(conf.network)) {
   stakingModelTasks = require('./stakingModelTasksOld.js')
 } else {
   stakingModelTasks = require('./stakingModelTasksV2')
