ec083fc751d39d21bf1edef7e4e7000e2e8233b6
sirpy <hadarr@gmail.com>
2021-12-21 13:35:48 +0200

    add: v2 protocol release

--- src/server/blockchain/AdminWallet.js
+++ src/server/blockchain/AdminWallet.js
@@ -1,9 +1,1 @@
-import conf from '../server.config'
-let AdminWallet
-// export default AdminWallet
-if (['production'].includes(conf.network)) {
-  AdminWallet = require('./AdminWalletOld').default
-} else {
-  AdminWallet = require('./AdminWalletV2').default
-}
-export default AdminWallet
+export * from './AdminWalletV2'
