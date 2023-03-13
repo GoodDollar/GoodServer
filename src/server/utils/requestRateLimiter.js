e8bc3e18037371aefb0bbee4ba9a80f28286a621
sirpy <hadarr@gmail.com>
2023-03-13 10:43:42 +0200

    add: userRateLimiter

--- src/server/utils/requestRateLimiter.js
+++ src/server/utils/requestRateLimiter.js
@@ -14,5 +14,6 @@ export const userRateLimiter = (limit, minutesWindow) =>
   rateLimit({
     windowMs: Math.round((minutesWindow || +m) * 60 * 1000), // minutes
     max: limit || +n, // limit each IP to n requests per windowMs
-    keyGenerator: request => request.user?.loggedInAs || request.ip
+    keyGenerator: request => request.user?.loggedInAs || request.ip,
+    message: 'per account rate limit exceeded'
   })
