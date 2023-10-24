50f8ab62f8cd74e8deb135c849640c5f00ae75ad
sirpy <hadarr@gmail.com>
2023-10-24 15:04:36 +0300

    fix: fv redacted fields

--- src/server/verification/utils/logger.js
+++ src/server/verification/utils/logger.js
@@ -3,15 +3,8 @@ import { ZoomAPIError } from './constants'
 const logAsErrors = [ZoomAPIError.HttpException, ZoomAPIError.UnexpectedException]
 
 export const enrollmentIdFields = ['enrollmentIdentifier', 'externalDatabaseRefID', 'identifier']
-export const faceSnapshotFields = [
-  'sessionId',
-  'faceScan',
-  'auditTrailImage',
-  'lowQualityAuditTrailImage',
-  'idScan',
-  'idScanFrontImage',
-  'idScanBackImage'
-]
-export const redactFieldsDuringLogging = ['faceMapBase64', 'auditTrailBase64', ...faceSnapshotFields]
+export const faceSnapshotFields = ['sessionId', 'faceScan', 'auditTrailImage', 'lowQualityAuditTrailImage']
+export const idscanFields = ['idScan', 'idScanFrontImage', 'idScanBackImage']
+export const redactFieldsDuringLogging = ['faceMapBase64', 'auditTrailBase64', ...faceSnapshotFields, ...idscanFields]
 
 export const shouldLogVerificaitonError = exception => logAsErrors.includes(exception.name)
