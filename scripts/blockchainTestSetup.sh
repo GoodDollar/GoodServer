523cec9932244115661eb352e518f32cc8775178
sirpy <hadarr@gmail.com>
2023-11-13 14:15:49 +0200

    fix: blockchain test setup

--- scripts/blockchainTestSetup.sh
+++ scripts/blockchainTestSetup.sh
@@ -3,7 +3,10 @@ pushd node_modules/@gooddollar/goodprotocol
 export CI=false
 export MNEMONIC='test test test test test test test test test test test junk'
 export ADMIN_MNEMONIC='test test test test test test test test test test test junk'
-yarn
+yarn set version 3.6.0
+echo "nodeLinker: node-modules" >> .yarnrc.yml
+yarn --immutable
+yarn runNode
 yarn deployTest
 yarn minimize
 popd
