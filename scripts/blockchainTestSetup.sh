d085c3426e170ddb922e8c2cfb1f413bef14efd0
sirpy <hadarr@gmail.com>
2023-11-14 18:05:52 +0200

    wip: fix e2e tests

--- scripts/blockchainTestSetup.sh
+++ scripts/blockchainTestSetup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-pushd node_modules/@gooddollar/goodprotocol
+cp -R node_modules/@gooddollar/goodprotocol /tmp
+pushd /tmp/goodprotocol
 export CI=false
 export MNEMONIC='test test test test test test test test test test test junk'
 export ADMIN_MNEMONIC='test test test test test test test test test test test junk'
 yarn set version 3.6.0
 echo "nodeLinker: node-modules" >> .yarnrc.yml
-yarn --immutable
-yarn runNode
+yarn install
+npx patch-package
+yarn runNode &
 yarn deployTest
 yarn minimize
 popd
+cp -R /tmp/goodprotocol/artifacts node_modules/@gooddollar/goodprotocol/
+cp -R /tmp/goodprotocol/releases node_modules/@gooddollar/goodprotocol/
