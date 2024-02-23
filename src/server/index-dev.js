105fcee99eb535279ee43981a1649c51efe85b9d
sirpy <hadar@gooddollar.org>
2024-02-23 10:53:49 +0200

    add: single file deploy

--- src/server/index-dev.js
+++ src/server/index-dev.js
@@ -1,10 +1,12 @@
 import throng from 'throng'
-import serverStart from './server-prod'
+import serverStart from './server-dev'
 const start = async workerId => {
+  console.log('server-dev')
   return serverStart(workerId)
 }
 if (process.env.WEB_CONCURRENCY > 1) {
   throng({ count: process.env.WEB_CONCURRENCY, lifetime: Infinity, worker: start, master: () => {} })
 } else {
+  console.log('server-dev start 0')
   start(0)
 }
