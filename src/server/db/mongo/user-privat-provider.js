146e2f9f45744b734a29f77af5bdfac27b1f2475
sirpy <hadar@gooddollar.org>
2024-05-02 10:52:31 +0300

    fix: get task

--- src/server/db/mongo/user-privat-provider.js
+++ src/server/db/mongo/user-privat-provider.js
@@ -190,7 +190,7 @@ class UserPrivate {
   async getTask(taskName, filters): Promise<DelayedTaskRecord> {
     const { taskModel } = this
     this.logger.debug('Getting task', { taskName, filters })
-    return taskModel.findOne({ taskName, ...filters })
+    return taskModel.findOne({ taskName, ...filters }).lean()
   }
   /**
    * Enqueue delayed task to the user's tasks queue
