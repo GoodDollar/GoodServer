c6972069c81301db2fc4f22f3267400f04aad065
sirpy <hadar@gooddollar.org>
2024-12-24 17:29:04 +0200

    fix: config schema

--- src/server/networks.js
+++ src/server/networks.js
@@ -12,19 +12,8 @@ export default once(() => {
       network_id: 1,
       web3Transport: 'HttpProvider',
       httpWeb3Provider: `https://rpc.ankr.com/eth,https://eth-rpc.gateway.pokt.network,https://cloudflare-eth.com,https://eth-mainnet.alchemyapi.io/v2/${alchemyKey}`,
-      websocketWeb3Provider: 'wss://mainnet.infura.io/ws'
-    },
-    42: {
-      network_id: 42,
-      web3Transport: 'HttpProvider',
-      httpWeb3Provider: `https://eth-kovan.alchemyapi.io/v2/${alchemyKey}`,
-      websocketWeb3Provider: 'wss://kovan.infura.io/ws'
-    },
-    3: {
-      network_id: 3,
-      web3Transport: 'HttpProvider',
-      httpWeb3Provider: `https://eth-ropsten.alchemyapi.io/v2/${alchemyKey}`,
-      websocketWeb3Provider: 'wss://ropsten.infura.io/ws'
+      websocketWeb3Provider: 'wss://mainnet.infura.io/ws',
+      explorer: ''
     },
     11155111: {
       network_id: 11155111,
@@ -32,25 +21,22 @@ export default once(() => {
       httpWeb3Provider: mainnetRpc
         ? mainnetRpc
         : `https://ethereum-sepolia-rpc.publicnode.com,https://rpc2.sepolia.org,https://eth-sepolia.public.blastapi.io,https://sepolia.drpc.org,https://sepolia.gateway.tenderly.co`,
-      websocketWeb3Provider: ''
-    },
-    5: {
-      network_id: 5,
-      web3Transport: 'HttpProvider',
-      httpWeb3Provider: `https://eth-goerli.alchemyapi.io/v2/${alchemyKey}`,
-      websocketWeb3Provider: 'wss://goerli.infura.io/ws'
+      websocketWeb3Provider: '',
+      explorer: ''
     },
     4447: {
       network_id: 4447,
       web3Transport: 'HttpProvider',
       httpWeb3Provider: 'http://localhost:8545/',
-      websocketWeb3Provider: 'ws://localhost:8545/'
+      websocketWeb3Provider: 'ws://localhost:8545/',
+      explorer: ''
     },
     31337: {
       network_id: 31337,
       web3Transport: 'HttpProvider',
       httpWeb3Provider: 'http://localhost:8545/',
-      websocketWeb3Provider: 'ws://localhost:8545/'
+      websocketWeb3Provider: 'ws://localhost:8545/',
+      explorer: ''
     },
     122: {
       network_id: 122,
