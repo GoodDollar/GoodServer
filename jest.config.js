957aa94815eb70a077f8d311ba525af173fa3c61
blueogin <grilucosy@gmail.com>
2026-02-20 07:27:09 -0500

    chore: fix formatting issue in jest configuration transformIgnorePatterns

--- jest.config.js
+++ jest.config.js
@@ -185,7 +185,7 @@ module.exports = {
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: [
     "/node_modules/(?!(@veramo|multiformats|key-did-resolver|@aws-sdk)/)", 
-    "\\.pnp\\.[^\\\/]+$",
+    "\\.pnp\\.[^\\\/]+$"
   ],
   
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
